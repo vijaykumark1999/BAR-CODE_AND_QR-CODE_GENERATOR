259e0eb16edd08adc2b81e590da20a2982d02302
VIJJU1999 <94538858+VIJJU1999@users.noreply.github.com>
2022-05-03 16:13:19 +0530

    Add project files.

---
+++ BAR-CODE_AND_QR-CODE_GENERATOR/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextQrcode = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button1.Location = New System.Drawing.Point(3, 377)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(114, 20)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Scan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button2.Location = New System.Drawing.Point(123, 377)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 20)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "BARCode-Generate"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button3.Location = New System.Drawing.Point(363, 377)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(114, 20)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Save"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button4.Location = New System.Drawing.Point(483, 377)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(113, 20)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Load"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 11)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(594, 336)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'TextQrcode
+        '
+        Me.TextQrcode.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextQrcode.Location = New System.Drawing.Point(1, 353)
+        Me.TextQrcode.Name = "TextQrcode"
+        Me.TextQrcode.Size = New System.Drawing.Size(594, 20)
+        Me.TextQrcode.TabIndex = 6
+        '
+        'Button5
+        '
+        Me.Button5.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button5.Location = New System.Drawing.Point(243, 377)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(114, 20)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "QRCode-Generate"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button6.Location = New System.Drawing.Point(363, 403)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(114, 20)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "DataBase"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button7.Location = New System.Drawing.Point(483, 403)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(114, 20)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "EXIT"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(604, 426)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextQrcode)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MinimumSize = New System.Drawing.Size(426, 297)
+        Me.Name = "Form1"
+        Me.Text = "   "
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextQrcode As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+End Class
---
+++ BAR-CODE_AND_QR-CODE_GENERATOR/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    Dim QR_Generator As New MessagingToolkit.QRCode.Codec.QRCodeEncoder
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Generator As New MessagingToolkit.Barcode.BarcodeEncoder
+        Generator.IncludeLabel = True
+        Generator.CustomLabel = TextQrcode.Text
+        Try
+            PictureBox1.Image = New Bitmap(Generator.Encode(MessagingToolkit.Barcode.BarcodeFormat.Code128C, TextQrcode.Text))
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Scanner As New MessagingToolkit.Barcode.BarcodeDecoder
+        Dim result As MessagingToolkit.Barcode.Result
+        Try
+            result = Scanner.Decode(New Bitmap(PictureBox1.Image))
+            TextQrcode.Text = result.Text
+            MsgBox("Succesfully Decoded-Code : " + result.Text)
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim SD As New SaveFileDialog
+        SD.Filter = "PNG File|*.png"
+        If SD.ShowDialog() = DialogResult.OK Then
+            Try
+                PictureBox1.Image.Save(SD.FileName, Imaging.ImageFormat.Png)
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim OD As New OpenFileDialog
+        OD.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+        If OD.ShowDialog() = DialogResult.OK Then
+            Try
+                PictureBox1.Load(OD.FileName)
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextQrcode.TextChanged
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim generate As New MessagingToolkit.Barcode.BarcodeEncoder
+        generate.BackColor = Color.White
+        generate.LabelFont = New Font("Arial", 7, FontStyle.Regular)
+        generate.IncludeLabel = True
+        generate.CustomLabel = TextQrcode.Text
+
+        Try
+            PictureBox1.Image = New Bitmap(generate.Encode(MessagingToolkit.Barcode.BarcodeFormat.QRCode, TextQrcode.Text))
+        Catch ex As Exception
+            PictureBox1.Image = Nothing
+        End Try
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Close()
+    End Sub
+End Class
